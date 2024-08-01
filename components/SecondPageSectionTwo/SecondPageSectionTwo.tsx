4101783c2df5788c5882c2f5d70a36ca2a56a016
Oyiga Victor <victoroyiga01@gmail.com>
2024-08-01 18:28:26 +0100

    update

--- components/SecondPageSectionTwo/SecondPageSectionTwo.tsx
+++ components/SecondPageSectionTwo/SecondPageSectionTwo.tsx
@@ -4,7 +4,8 @@ import SePictur from '@/app/assets/SecondPageImage/Group 13937.svg'
 
 const SecondPageSectionTwo = () => {
     return (
-        <div className='flex flex-col md:flex-row item-center w-full justify-evenly'>
+        <div className='flex flex-col w-full'>
+            <div className="flex flex-col md:flex-row item-center w-full justify-evenly">
             <div className='w-[300px] h-[300px] md:w-[600px] md:h-[600px] sm:ml-10'>
                 <Image src={SePictur} alt="picture" />
             </div>
@@ -36,6 +37,17 @@ const SecondPageSectionTwo = () => {
                     <p className='items-center'>adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
             </div>
+
+
+            </div>
+                <div className='w-[100%] text-gray-500'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+
 
         </div>
     )
